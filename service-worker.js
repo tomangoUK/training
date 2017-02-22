cd4a4c7b5c7af6ad87c4ca3154b513fa1662d0a3
Trys Mudford <trysmudford@gmail.com>
2017-02-22 11:23:41 +0000

    Event.waitUntil order

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 
 importScripts('/static/js/async-waituntil.js')
 
-const cacheNameStatic = 'training-static-v14'
+const cacheNameStatic = 'training-static-v15'
 const cacheNameVideo = 'training-videos-v2'
 const cacheNameExternal = 'training-external-v1'
 const cacheNamePrefetch = 'training-prefetch-v1'
@@ -62,6 +62,7 @@ function clearCaches() {
 self.addEventListener('activate', event => {
   event.waitUntil(clearCaches()
     .then(function() {
+      self.clients.claim()
       event.waitUntil(
         clients.matchAll().then(clients => {
           clients.forEach(client => {
