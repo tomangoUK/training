7ff0ccdd007f980de747f327f14fb82e3ac82158
Trys Mudford <trysmudford@gmail.com>
2017-11-22 08:45:52 +0000

    Service worker range responses

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
 /* eslint-env worker, serviceworker */
-/* global goog */
+/* global goog, RangedResponse */
 
 importScripts('/static/js/async-waituntil.js')
+importScripts('/static/js/ranged-response.js')
 
-const cacheNameStatic = 'training-static-v4'
+const cacheNameStatic = 'training-static-v5'
 const cacheNameVideo = 'training-videos-v1'
 const cacheNameExternal = 'training-external-v1'
 const videosMp4 = [
@@ -172,93 +173,63 @@ self.addEventListener('fetch', event => {
     return
   }
 
-  if (event.request.headers.get('range')) {
-    var pos = Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1])
+  RangedResponse.canHandle(request).then(canHandleRequest => {
+    if (canHandleRequest) {
+      return RangedResponse.create(request)
+    }
 
-    event.respondWith(
-      caches.open(cacheNameVideo)
-      .then(function(cache) {
-        return cache.match(event.request.url)
-      }).then(function(res) {
-        if (!res) {
-          return fetch(event.request)
-          .then(res => {
-            return res.arrayBuffer()
-          })
+    caches.match(event.request)
+      .then(function (responseFromCache) {
+        if (responseFromCache && (request.url.indexOf('.mp4') === -1 || request.url.indexOf('.webm') === -1) ) {
+          let requestUrl = request.url.indexOf('?') === -1 ? `${request.url}?${Math.random()}` : `${request.url}&${Math.random()}` 
+          fetch(requestUrl)
+            .then( responseFromFetch => {
+              caches.open(cacheNameStatic)
+              .then( cache => {
+                cache.put(request, responseFromFetch)
+              })
+            })
+          return responseFromCache
         }
-        return res.arrayBuffer()
-      }).then(function(ab) {
-        return new Response(
-          ab.slice(pos),
-          {
-            status: 206,
-            statusText: 'Partial Content',
-            headers: [
-              ['Content-Range', 'bytes ' + pos + '-' +
-                (ab.byteLength - 1) + '/' + ab.byteLength]]
-          })
-      }))
 
-  } else {
+        var fetchRequest = event.request.clone()
 
-    event.respondWith(
-      caches.match(event.request)
-        .then(function (responseFromCache) {
+        return fetch(fetchRequest).then(
+          function (response) {
 
-          if (responseFromCache && (request.url.indexOf('.mp4') === -1 || request.url.indexOf('.webm') === -1) ) {
-            let requestUrl = request.url.indexOf('?') === -1 ? `${request.url}?${Math.random()}` : `${request.url}&${Math.random()}` 
-            event.waitUntil(
-                fetch(requestUrl)
-                .then( responseFromFetch => {
-                  caches.open(cacheNameStatic)
-                  .then( cache => {
-                    cache.put(request, responseFromFetch)
-                  })
-                })
-            )
-            return responseFromCache
-          }
-
-          var fetchRequest = event.request.clone()
-
-          return fetch(fetchRequest).then(
-            function (response) {
-
-              var cacheName = false
-              if (response.type === 'basic' && response.status === 200) {
-                if (url.href.indexOf('.mp4') !== -1 || url.href.indexOf('.webm') !== -1) {
-                  cacheName = cacheNameVideo
-                } else {
-                  cacheName = cacheNameStatic
-                }
+            var cacheName = false
+            if (response.type === 'basic' && response.status === 200) {
+              if (url.href.indexOf('.mp4') !== -1 || url.href.indexOf('.webm') !== -1) {
+                cacheName = cacheNameVideo
               } else {
-                cacheName = cacheNameExternal
+                cacheName = cacheNameStatic
               }
-
-              if (cacheName) {
-                var responseToCache = response.clone()
-
-                caches.open(cacheName)
-                  .then(function (cache) {
-                    var cacheRequest = event.request.clone()
-                    cache.put(cacheRequest, responseToCache)
-                  })
-              }
-
-              return response
-            } 
-          )
-          .catch( () => {
-            if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-              return caches.match(request)
-                .then( response => response || caches.match('/offline') )
-            } else if (request.headers.get('Accept').indexOf('image') !== -1) {
-              return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store'}})
+            } else {
+              cacheName = cacheNameExternal
             }
-          })
 
+            if (cacheName) {
+              var responseToCache = response.clone()
+
+              caches.open(cacheName)
+                .then(function (cache) {
+                  var cacheRequest = event.request.clone()
+                  cache.put(cacheRequest, responseToCache)
+                })
+            }
+
+            return response
+          } 
+        )
+        .catch( () => {
+          if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+            return caches.match(request)
+              .then( response => response || caches.match('/offline') )
+          } else if (request.headers.get('Accept').indexOf('image') !== -1) {
+            return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store'}})
+          }
         })
-      )
-  }
 
+      })
+  })
 })
